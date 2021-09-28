b5e6259846f7c9eb4face38eb91dd2de628b1703
Jessica Pergentino <jessicapergentino@hotmail.com>
2021-09-28 16:58:11 -0300

    Finished React Native Component Playbook

--- AwesomeProject/src/containers/Home.js
+++ AwesomeProject/src/containers/Home.js
@@ -100,4 +100,6 @@ function Home({navigation}) {
   );
 }
 
+
+
 export default Home;
--- AwesomeProject/src/containers/Story.js
+++ AwesomeProject/src/containers/Story.js
@@ -11,7 +11,9 @@ function Story() {
       contentContainerStyle={styles.storyInnerContainer}
       stickyHeaderIndices={[0, 1]}
       indicatorStyle="white"
-      pagingEnabled={true}>
+      pagingEnabled={true}
+      scrollsToTop={true}
+      bounces={true}>
       <Image
         style={styles.storyImageSmall}
         source={require('../images/shutterstock_211091626.png')}
@@ -31,7 +33,25 @@ function Story() {
           }>
           <Text style={styles.sectionDescriptionStory}>Go To End</Text>
         </Pressable>
+        <Text style={styles.sectionDescriptionStory}>
+          This is going to be a lengthy story on how this conference started
+        </Text>
 
+        <Text style={styles.sectionDescriptionStory}>
+          Ex amet elit anim qui consectetur fugiat consequat dolor occaecat.
+          Pariatur eu duis dolore do commodo nisi consequat enim est pariatur
+          eiusmod adipisicing esse labore ut. Laborum et ex aliqua esse Lorem
+          aliqua. Mollit nulla nostrud enim ut proident commodo deserunt
+          occaecat ex elit ex est.\nEsse enim nulla dolore sint incididunt enim
+          eu eu aute aliqua laboris deserunt eiusmod. Reprehenderit et proident
+          nisi laborum ex minim velit. Aliqua laboris et nulla laborum amet sint
+          irure ad tempor. Nulla do officia labore. Ut amet aute ex proident.
+          Enim sint aliqua occaecat voluptate adipisicing dolore proident Ex
+          amet elit anim qui consectetur fugiat consequat dolor occaecat.
+          Pariatur eu duis dolore do commodo nisi consequat enim est pariatur
+          eiusmod adipisicing esse labore ut. Laborum et ex aliqua esse Lorem
+          aliqua. Mollit nulla nostrud enim ut proident commodo deserunt
+        </Text>
         <Text style={styles.sectionDescriptionStory}>
           Ex amet elit anim qui consectetur fugiat consequat dolor occaecat.
           Pariatur eu duis dolore do commodo nisi consequat enim est pariatur
@@ -63,7 +83,6 @@ function Story() {
           eiusmod adipisicing esse labore ut. Laborum et ex aliqua esse Lorem
           aliqua. Mollit nulla nostrud enim ut proident commodo deserunt
         </Text>
-
         <Text style={styles.sectionDescriptionStory}>
           Ex amet elit anim qui consectetur fugiat consequat dolor occaecat.
           Pariatur eu duis dolore do commodo nisi consequat enim est pariatur
@@ -79,30 +98,10 @@ function Story() {
           eiusmod adipisicing esse labore ut. Laborum et ex aliqua esse Lorem
           aliqua. Mollit nulla nostrud enim ut proident commodo deserunt
         </Text>
-
-        <Text style={styles.sectionDescriptionStory}>
-          Ex amet elit anim qui consectetur fugiat consequat dolor occaecat.
-          Pariatur eu duis dolore do commodo nisi consequat enim est pariatur
-          eiusmod adipisicing esse labore ut. Laborum et ex aliqua esse Lorem
-          aliqua. Mollit nulla nostrud enim ut proident commodo deserunt
-          occaecat ex elit ex est.\nEsse enim nulla dolore sint incididunt enim
-          eu eu aute aliqua laboris deserunt eiusmod. Reprehenderit et proident
-          nisi laborum ex minim velit. Aliqua laboris et nulla laborum amet sint
-          irure ad tempor. Nulla do officia labore. Ut amet aute ex proident.
-          Enim sint aliqua occaecat voluptate adipisicing dolore proident Ex
-          amet elit anim qui consectetur fugiat consequat dolor occaecat.
-          Pariatur eu duis dolore do commodo nisi consequat enim est pariatur
-          eiusmod adipisicing esse labore ut. Laborum et ex aliqua esse Lorem
-          aliqua. Mollit nulla nostrud enim ut proident commodo deserunt
-        </Text>
-
         <Text style={styles.sectionDescriptionStory}>End of this story!</Text>
-
         <Pressable
-          onPress={() =>
-            this.scroll.scrollTo({x: 0, y: 0, animated: true})
-          }>
-          <Text style={styles.sectionDescriptionStory}>Go To Top</Text>
+          onPress={() => this.scroll.scrollTo({x: 0, y: 0, animated: true})}>
+          <Text style={styles.sectionDescriptionStory}>Go to Top</Text>
         </Pressable>
       </View>
     </ScrollView>
